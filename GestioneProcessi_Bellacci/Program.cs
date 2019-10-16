0f8b187842d0aaf7834d1267a196f50ae7626321
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 09:39:39 +0200

    inserito un blocco di istruzione che permette di chiudere i file di testo

--- GestioneProcessi_Bellacci/Program.cs
+++ GestioneProcessi_Bellacci/Program.cs
@@ -16,11 +16,31 @@ namespace GestioneProcessi_Bellacci
             //avvia un processo
             Process.Start("Notepad.exe");
 
-            //avvia un altro processo corrispondente al file TextFile1.txt creato in precedenza
+            //avvia un processo corrispondente al file TextFile1.txt creato in precedenza
             Process.Start("Notepad.exe", @"C:\Users\studenti\Desktop\GestioneProcessi_Bellacci\GestioneProcessi_Bellacci\TextFile1.txt");
 
             //avvia un altro processo corrispondente ad una pagina di chrome
             Process.Start("Chrome.exe", @"http://secondarieclago.it/");
+
+            //la variabile app assume il valore di un nuovo processo
+            var app = new Process();
+            app.StartInfo.FileName = @"Notepad.exe";
+            app.Start();
+
+            //avvia un altro processo corrispondente al file TextFile1.txt creato in precedenza
+            app.StartInfo.Arguments= @"C:\Users\studenti\Desktop\GestioneProcessi_Bellacci\GestioneProcessi_Bellacci\TextFile1.txt";
+            app.Start();
+
+            //chiude tutti i processi con notepad (minuscolo)
+            var process = Process.GetProcesses();
+            foreach (var p in process)
+            {
+                if (p.ProcessName == "notepad")
+                {
+                    p.Kill();
+                }
+            }
+            Console.WriteLine("Programma Terminato");
 
             Console.ReadLine();
         }
